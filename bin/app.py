3b94d9ea6b84170c68a32f6cb97344478939ebf9
João Vaz <joao.guimaraes.alves.vaz@gmail.com>
2020-08-16 20:16:26 -0300

    Major optimizations and refactoring

--- bin/app.py
+++ bin/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+import sys
 
-txt = list(input("Text to ironize: "))
+txt = list(sys.argv[1] if len(sys.argv) > 1 else input("Text to ironize: "))
 for i in range(len(txt)):
     if txt[i] == ' ':
         continue
-        
+
     txt[i] = txt[i].upper() if i % 2 == 0 else txt[i].lower()
-print("\n\n", "".join(txt))
+print("".join(txt))
